bf66d68e3b0fd91c8a1aa6c48a8ed1edeaa4f579
Chethan-m <chethanmkumar19@gmail.com>
2021-06-05 13:11:00 +0530

    Modifying HelloWorld my first program to remote repository

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,11 @@ public class
 	public static void  main (Strings args[])
 
   {
-
+    String aa = "Hassan";
+    int a =5:
+    int b =10:
+    int a+b =15:
+ system.out.println("the result of a+b is"+a+b);    
   }
 
 }  
